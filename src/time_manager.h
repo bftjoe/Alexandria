e38172759a695326e2c96238ae105f7bd91033f1
Joseph Huang <josephshuang@gmail.com>
2025-09-08 08:02:57 -0400

    remove time manager.cpp
    Bench: 8600154

--- src/time_manager.h
+++ src/time_manager.h
@@ -1,10 +1,70 @@
 #pragma once
 
+#include "position.h"
+#include "search.h"
+#include <algorithm>
+#include "misc.h"
+
 struct SearchInfo;
 struct ThreadData;
 
-void Optimum(SearchInfo* info, int time, int inc);
-[[nodiscard]] bool StopEarly(const SearchInfo* info);
-[[nodiscard]] bool TimeOver(const SearchInfo* info);
-[[nodiscard]] bool NodesOver(const SearchInfo* info);
-void ScaleTm(ThreadData* td, const int bestMoveStabilityFactor, const int evalStabilityFactor);
+// Calculate how much time to spend on searching a move
+inline void Optimum(SearchInfo* info, int time, int inc) {
+    // If ccrl sent us a negative time just assume we have a workable amount of time to search for a move
+    if (time < 0) time = 1000;
+    // Reserve some time overhead to avoid timing out in the engine-gui communication process
+    const int safety_overhead = std::min(25, time / 2);
+    // if we received a movetime command we need to spend exactly that amount of time on the move, so we don't scale
+    if (info->movetimeset) {
+        info->stoptimeMax = info->starttime + time - safety_overhead;
+        info->stoptimeOpt = info->starttime + time - safety_overhead;
+        return;
+    }
+    const bool cyclicTC = info->movestogo != 0;
+    const int movesToGo  = cyclicTC ? std::min(info->movestogo, 50) : 50;
+    const auto timeLeft = std::max(1, time + inc * (movesToGo - 1) - safety_overhead * (2 + movesToGo));
+    double optScale = 0;
+    if(cyclicTC){
+        optScale = std::min(0.90 / movesToGo, 0.88 * time / double(timeLeft));
+    }
+    else{
+        optScale = std::min(optScaleFixed() / 1000.0, optScaleTimeLeft() / 1000.0 * time / double(timeLeft));
+    }
+    // optime is the time we use to stop if we just cleared a depth
+    const auto optime =  optScale * timeLeft;
+    info->stoptimeBaseOpt = optime;
+    info->stoptimeOpt = info->starttime + info->stoptimeBaseOpt;
+    // Never use more than 76% of the total time left for a single move
+    const auto maxtime = 0.76 * time - safety_overhead;
+    info->stoptimeMax = info->starttime + maxtime;
+}
+
+[[nodiscard]] inline bool StopEarly(const SearchInfo* info) {
+    // check if we used all the nodes/movetime we had or if we used more than our lowerbound of time
+    return (info->timeset || info->movetimeset) && GetTimeMs() > info->stoptimeOpt;
+}
+
+[[nodiscard]] inline bool NodesOver(const SearchInfo* info) {
+    // check if we used all the nodes/movetime we had or if we used more than our lowerbound of time
+    return info->nodeset && info->nodes >= info->nodeslimit;
+}
+
+[[nodiscard]] inline bool TimeOver(const SearchInfo* info) {
+    // check if more than Maxtime passed and we have to stop
+    return NodesOver(info) || ((info->timeset || info->movetimeset)
+                               && ((info->nodes & 1023) == 1023)
+                               && GetTimeMs() > info->stoptimeMax);
+}
+
+inline void ScaleTm(ThreadData* td, const int bestMoveStabilityFactor, const int evalStabilityFactor) {
+    const double bestmoveScale[5] = {bmScale1() / 100.0, bmScale2() / 100.0, bmScale3() / 100.0, bmScale4() / 100.0, bmScale5() / 100.0};
+    const double evalScale[5] = {evalScale1() / 100.0, evalScale2() / 100.0, evalScale3() / 100.0, evalScale4() / 100.0, evalScale5() / 100.0};
+    const int bestmove = GetBestMove();
+    // Calculate how many nodes were spent on checking the best move
+    const double bestMoveNodesFraction = static_cast<double>(nodeSpentTable[FromTo(bestmove)]) / static_cast<double>(td->info.nodes);
+    const double nodeScalingFactor = (nodeTmBase() / 100.0 - bestMoveNodesFraction) * (nodeTmMultiplier() / 100.0);
+    const double bestMoveScalingFactor = bestmoveScale[bestMoveStabilityFactor];
+    const double evalScalingFactor = evalScale[evalStabilityFactor];
+    // Scale the search time based on how many nodes we spent and how the best move changed
+    td->info.stoptimeOpt = std::min<uint64_t>(td->info.starttime + td->info.stoptimeBaseOpt * nodeScalingFactor * bestMoveScalingFactor * evalScalingFactor, td->info.stoptimeMax);
+}
